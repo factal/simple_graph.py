f653efbb4afe3a566636f4d5a842ed7f300257d2
factal <31815263+factal@users.noreply.github.com>
2021-04-29 18:23:25 +0900

    Update simple_graph.py
    ・コーディングスタイルの統一
    ・どうやらグラフ理論の文脈では vertexes よりも vertices の方が好まれるっぽい？
    ・Vertex 及び Edge クラスに self.graph 変数を追加し、どの Graph に属しているのかを記憶させるようにした
    ・Graph 内における Vertex と Edge の関係性を隣接行列による表現から隣接リストを改変した方式に変更した
    ・Graph.init_edges(), Graph.update_connection_info() を削除し、機能を Graph.add_edge() に統合した
    ・Graph.generate_adj_matrix() の追加
    ・コメント及びアノテーションの追加

--- simple_graph.py
+++ simple_graph.py
@@ -29,173 +29,207 @@ class pq:
                 self.data[index] = tuple(data)
                 break
 
-class vertex:
-    def __init__(self, index: int, content=None):
-        self.index = index # グラフ内でのインデックス
+class Vertex:
+    def __init__(self, graph, index: int, content=None):
+        self.graph = graph # 頂点が属するグラフ
+        self.index = index # グラフ内でのインデックス番号, 隣接行列の構築などに用いる
         self.content = content # 頂点が保持する情報
-        self.adj = [] # 隣接する頂点
+        self.adj_vertices = [] # 隣接する頂点 
+        self.connected_edges = [] # 頂点と繋がっている辺 (始点となる辺のみ保持)
     
     def __str__(self):
         return str(self.index)
-    
-    def init_adj(self, graph):
-        # 隣接行列 graph.matrix で隣接リスト vertex.adj を初期化
-        
-        for index_adj, edge in enumerate(graph.adj_matrix[self.index]):
-            if self.index == index_adj:
-                continue
-            elif edge != float('inf'):
-                self.adj.append(graph.vertexes[index_adj])
 
-class edge:
-    def __init__(self, start: vertex, end: vertex, weight=1):
+class Edge:
+    def __init__(self, graph, start: Vertex, end: Vertex, weight):
+        self.graph = graph # 辺が属するグラフ
         self.start = start # 始点
         self.end = end # 終点
         self.weight = weight # 重み
-
-class graph:
-    def __init__(self, size: int=0): # size: 頂点数
-        self.adj_matrix = [[float('inf')] * size for _ in range(size)] # 隣接行列
-        self.vertexes = [vertex(index) for index in range(size)] # 頂点集合
-        self.edges = [] # 辺集合
+        # self.adj_edges = [] # 隣接辺, 隣接情報の更新の際の計算量に関する問題を解決できなかったので現状サポートしていません
     
     def __str__(self):
-        return '\n'.join(map(str, self.adj_matrix))
-    
-    def init_edges(self, edges: list, one_index=False):
-        # self.edges 更新
-        # edges の凡例: [[始点のインデックス, 終点のインデックス, 重み], [始点のインデックス, 終点のインデックス, 重み], ...] または [[始点のインデックス, 終点のインデックス], [始点のインデックス, 終点のインデックス], ...]
-        
-        if len(edges[0]) == 2: # unweighted
-            for start, end in edges:
-                self.edges.append(edge(start - one_index, end - one_index))
-        
-        elif len(edges[0]) == 3: # weighted
-            for start, end, weight in edges:
-                self.edges.append(edge(start - one_index, end - one_index, weight))
+        representation = '(' + str(self.start.index) + ', ' + str(self.end.index) +'): '+ str(self.weight)
+        return representation
 
-    def update_connection_info(self, is_directed=False):
-        # self.edges を元に頂点/辺の接続情報を更新
-        # is_directed: Trueのとき 有向グラフ, Falseのとき 無向グラフ
-        
-        # 隣接行列更新
-        if is_directed:
-            for e in self.edges:
-                self.adj_matrix[e.start][e.end] = e.weight        
-        else:
-            for e in self.edges:
-                self.adj_matrix[e.start][e.end] = e.weight
-                self.adj_matrix[e.end][e.start] = e.weight
-        
-        # vertex の隣接リスト更新
-        for v in self.vertexes:
-            v.init_adj(self)
+class Graph:
+    def __init__(self, size: int=0, directed: bool=False): # size: 頂点数
+        self.vertices = [Vertex(self, i) for i in range(size)] # 頂点集合
+        self.edges = {} # 辺集合, {(始点, 終点): Edge, ...}
+        # self.adj_matrix = [] # 隣接行列, メモリのオーバーヘッドを考慮してその都度 Graph.generate_adj_matrix() で生成するようにしました
+        self._vertices_size = size # 頂点集合の大きさ
+        self.directed = directed # 有向グラフかどうか
     
-    def get_weight(self, start: vertex, end: vertex) -> int:
-        # vertex -> weight
-        
-        return self.adj_matrix[start.index][end.index]  
+    def add_vertex(self, content=None) -> None:
+        # 頂点の追加
+
+        self.vertices.append(Vertex(self, self._vertices_size, content))
+        self._vertices_size += 1
     
-    def get_connected_vertexes(self, e: edge):
-        # edge -> vertex
+    def add_edge_from_index(self, start_index: int, end_index: int, weight: float=1) -> None:
+        start_vertex = self.vertices[start_index]
+        end_vertex = self.vertices[end_index]
+
+        self.add_edge(start_vertex, end_vertex, weight)
+    
+    def add_edge(self, start: Vertex, end: Vertex, weight: float=1) -> None:
+        # 辺の追加
+        # Vertex.adj_vertices, Vertex.connected_edges も更新します
+
+        new_edge = Edge(self, start, end, weight) 
+
+        self.edges.setdefault((start, end), [])
+        self.edges[(start, end)].append(new_edge)
+
+        # 接続情報更新 (計算量削減および良い実装が思いつかなかったため、クラスメソッドに分けずここに直接書いています)
+        start.adj_vertices.append(end)
+        start.connected_edges.append(new_edge)
+
+        # 無向グラフ用
+        if self.directed:
+            edge_reversed = Edge(self, end, start, weight)
+            end.adj_vertices.append(start)
+            self.edges.setdefault((end, start), [])
+            self.edges[(end, start)].append(edge_reversed)
+            end.connected_edges.append(edge_reversed)
+
+
+    def get_weight_from_vertices(self, start: Vertex, end: Vertex) -> float:
+        # Vertex の組から重みを取得, 多重辺が存在する場合は最小の重みを返す
+        # O(E)?
+    
+        connected_edges = self.edges[(start, end)]
+        return min([e.weight for e in connected_edges])
+    
+    def get_connected_vertices(self, new_edge: Edge) -> tuple:
+        # Edge に接続された Vertex を (始点, 終点) のタプルで返す
         
-        return (self.vertexes[e.start], self.vertexes[e.end])
-            
+        return (self.vertices[new_edge.start], self.vertices[new_edge.end])
+    
+    def generate_adj_matrix(self) -> list:
+        # 隣接行列生成
+        # O(E)?
+
+        adj_matrix = [[0] * self._vertices_size for _ in range(self._vertices_size)] # init
+
+        for start, end in self.edges.keys():
+            start_index = start.index
+            end_index = end.index
+
+            adj_matrix[start_index][end_index] = self.get_weight_from_vertices(start, end)
+        
+        return adj_matrix
+
+
+
 # --- algorithms --- #
 
 
 
-def dfs(graph: graph, root: vertex, visit_all_vertexes=False) -> dict:
+def dfs(graph: Graph, root: Vertex, visit_all_vertices=False) -> dict:
     # O(V+E)
-    # visit_all_vertexes: すべての頂点を探索するかどうか (True のとき pred は深さ優先木探索結果の深さ優先森を記録する)
+    # visit_all_vertices: すべての頂点を探索するかどうか (True のとき pred は深さ優先木探索結果の深さ優先森を記録する)
     # 入力: グラフ graph, 探索の始点 root
     # 出力: 深さ優先順序において前の頂点を記録した辞書 pred
     
-    def _dfs_visit(current: vertex):
-        label[current] = 'Searching'
-        for next in current.adj:
-            if label[next] == 'Unsearched':
+    def __dfs_visit(current: Vertex) -> None:
+        label[current] = 'searching'
+        for next in current.adj_vertices:
+            if label[next] == 'unsearched': # 未訪問地点を見つけ、その方向に進む
                 pred[next] = current
-                _dfs_visit(next)
-        label[current] = 'Searched' 
+                __dfs_visit(next)
+        label[current] = 'searched' 
     
-    pred = {v: None for v in graph.vertexes}
-    label = {v: 'Unsearched' for v in graph.vertexes}
+    # init
+    pred = {v: None for v in graph.vertices}
+    label = {v: 'unsearched' for v in graph.vertices}
     
-    if visit_all_vertexes:
+    if visit_all_vertices:
         for key, state in label.items():
-            if state == 'Unsearched':
-                _dfs_visit(key)
+            if state == 'unsearched': 
+                __dfs_visit(key)
+
     else:
-        _dfs_visit(root)
+        __dfs_visit(root)
     
     return pred
     
-def bfs(graph: graph, root: vertex) -> tuple:
+def bfs(graph: Graph, root: Vertex) -> tuple:
     # O(V+E)
     # 入力: グラフ graph, 探索の始点 root
     # 出力: (幅優先順序において前の頂点を記録した辞書 pred, 始点からの距離の辞書 distance)
     
-    pred = {v: None for v in graph.vertexes}
-    label = {v: 'Unsearched' for v in graph.vertexes}
-    distance = {v: float('inf') for v in graph.vertexes}
+    # init
+    pred = {v: None for v in graph.vertices}
+    label = {v: 'unsearched' for v in graph.vertices}
+    distance = {v: float('inf') for v in graph.vertices}
     
-    label[root] = 'Searching'
+    label[root] = 'searching'
     distance[root] = 0
     visited = deque([root])
     
     while len(visited) != 0:
         current = visited.popleft()
-        for next in current.adj:
-            if label[next] == 'Unsearched':
+        for next in current.adj_vertices:
+            if label[next] == 'unsearched':
                 pred[next] = current
-                label[next] = 'Searching'
+                label[next] = 'searching'
                 distance[next] = distance[current] + 1
 
                 visited.append(next)
-        label[current] = 'Searched'
+        label[current] = 'searched'
     
     return (pred, distance)
 
-def dijkstra(graph: graph, root) -> tuple:
+def dijkstra(graph: Graph, root) -> tuple:
     # single source shortest path, O((V+E)*logV)
     # 入力: 有向重み付きグラフ graph (任意の重みは非負), 始点 root
     # 出力: (始点から各頂点に至る実際の最短経路の再計算に用いる辞書 pred, 始点からの距離の辞書 distance)
     
-    pred = {v: None for v in graph.vertexes}
-    distance = {v: float('inf') for v in graph.vertexes}
+    # init
+    pred = {v: None for v in graph.vertices}
+    distance = {v: float('inf') for v in graph.vertices}
     distance[root] = 0
     
     queue = pq()
-    for v in graph.vertexes:
-        queue.push(v, distance[v])
+    for v in graph.vertices:
+        queue.push(v, distance[v]) # 最短経路距離を優先度として全頂点を優先度付きキュー queue に push
     
     while len(queue) != 0:
-        current = queue.pop()
-        for next in current.adj:
-            length = distance[current] + graph.get_weight(current, next)
+        current = queue.pop() # 始点への最短経路の頂点を取り出す
+
+        for next in current.adj_vertices:
+            length = distance[current] + graph.get_weight_from_vertices(current, next)
+
             if length < distance[next]:
+                # current から next へのより短い経路で queue を更新し記録
+
                 queue.set_priority(next, length)
                 distance[next] = length
                 pred[next] = current
     
     return (pred, distance)
 
-def bellman_ford(graph: graph, root: vertex):
+def bellman_ford(graph: Graph, root: Vertex) -> tuple:
     # single source shortest path, O(V*E)
     # 入力: 有向重み付きグラフ graph (重みの総和が負となる閉路が存在してはならない), 始点 root
     # 出力: (始点から各頂点に至る実際の最短経路の再計算に用いる辞書 pred, 始点からの距離の辞書 distance)
     
-    pred = {v: None for v in graph.vertexes}
-    distance = {v: float('inf') for v in graph.vertexes}
+    # init
+    pred = {v: None for v in graph.vertices}
+    distance = {v: float('inf') for v in graph.vertices}
     distance[root] = 0
     
-    for i in range(len(graph.vertexes)):
+    for i in range(len(graph.vertices)):
         for e in graph.edges:
-            current, next = graph.get_connected_vertexes(e)
-            length = distance[current] + graph.get_weight(current, next)
+            current, next = graph.get_connected_vertices(e)
+            length = distance[current] + graph.get_weight_from_vertices(current, next)
+
             if length < distance[next]:
-                if i == len(graph.vertexes):
+                # current から next へのより短い経路を記録
+
+                if i == len(graph.vertices):
+                    # Bellman Ford 法は |V|-1 回の走査で最短経路を算出できるため、|V| 回目の経路の更新は重みの総和が負の閉路が存在することを意味する
                     print('found a cycle with negative weight')
                 
                 distance[next] = length
@@ -203,52 +237,55 @@ def bellman_ford(graph: graph, root: vertex):
     
     return (pred, distance)
             
-def warshall_floyd(graph: graph):
+def warshall_floyd(graph: Graph) -> tuple:
     # all pair shortest path, O(V^3)
     # 入力: 有向重み付きグラフ graph (任意の重みは正)
     # 出力: (各頂点間の最短経路の再計算に用いる2次元辞書 pred, 各頂点から全頂点への距離の2次元辞書 distance)
     
+    # init
     pred = {}
     distance = {}
     
-    for current in graph.vertexes:
-        pred[current] = {v: None for v in graph.vertexes}
-        distance[current] = {v: float('inf') for v in graph.vertexes}
+    for current in graph.vertices:
+        pred[current] = {v: None for v in graph.vertices}
+        distance[current] = {v: float('inf') for v in graph.vertices}
         distance[current][current] = 0
         
-        for next in current.adj:
-            distance[current][next] = graph.get_weight(current, next)
+        for next in current.adj_vertices:
+            distance[current][next] = graph.get_weight_from_vertices(current, next)
             pred[current][next] = current
         
-    for k in graph.vertexes:
-        for u in graph.vertexes:
-            for v in graph.vertexes:
+    for k in graph.vertices:
+        for u in graph.vertices:
+            for v in graph.vertices:
                 length = distance[u][k] + distance[k][v]
                 if length < distance[u][v]:
+                    # current から next へのより短い経路を記録
+
                     distance[u][v] = length
-                    pred[u][v] = pred[k][v]
+                    pred[u][v] = pred[k][v] # 新たに得られた1つ前のリンクを記録
     
     return (pred, distance)
 
-def prim(graph: graph, root: vertex) -> dict:
+def prim(graph: Graph, root: Vertex) -> dict:
     # MST, O((V+E)logV)
     # 入力: 無向グラフ graph, 探索を開始する頂点 root
     # 出力: pred に収められたMST (MSTの根は -1)
     
     # init
-    pred = {v: None for v in graph.vertexes}
+    pred = {v: None for v in graph.vertices}
     pred[root] = -1
-    distance = {v: float('inf') for v in graph.vertexes}
+    distance = {v: float('inf') for v in graph.vertices}
     distance[root] = 0
     
     queue = pq()
-    for v in graph.vertexes:
+    for v in graph.vertices:
         queue.push(v, distance[v])
     
     while len(queue) != 0:
-        current = queue.pop() # 距離が最小の頂点を取得
-        for next in current.adj:
-            weight = graph.get_weight(current, next)
+        current = queue.pop() # 計算された距離が最小の頂点を取得
+        for next in current.adj_vertices:
+            weight = graph.get_weight_from_vertices(current, next)
             if weight < distance[next]:
                 pred[next] = current
                 distance[next] = weight # next の推定コスト更新
@@ -256,46 +293,42 @@ def prim(graph: graph, root: vertex) -> dict:
     
     return pred
 
-def make_mst(original: graph, pred: dict) -> graph:
-    # prim で得た pred から MST の graph を作成
+def make_mst(original: Graph, pred: dict) -> Graph:
+    # prim() で得た pred から MST の graph を作成
     
-    mst = graph(len(original.vertexes))
-    mst.vertexes = original.vertexes
+    mst = Graph(len(original.vertices), directed=False) # init
     
-    edges = []
     for end, start in pred.items():
-        if isinstance(start, vertex):
-            weight = original.get_weight(start, end)
-            edges.append([start.index, end.index, weight])
-    
-    mst.init_edges(edges)
-    mst.update_connection_info(is_directed=True)
+        if isinstance(start, Vertex):
+            weight = original.get_weight_from_vertices(start, end)
+            mst.add_edge_from_index(start.index, end.index, weight)
     
     return mst
 
 # --- テストコード --- #
 
-test_graph = graph(5) # 5頂点のグラフを作成
-test_edges = [[0, 1, 2], [0, 4, 4], [1, 2, 3], [2, 4, 1], [2, 3, 5],  [3, 0, 8], [3, 4, 7], [4, 3, 7]] # 辺
-test_graph.init_edges(test_edges)
-test_graph.update_connection_info(is_directed=False) # 辺情報更新
+test_graph = Graph(5, directed=True) # 5頂点のグラフを作成
+test_edges = [[0, 1, 2], [0, 4, 4], [1, 2, 3], [2, 4, 1], [2, 3, 5],  [3, 0, 8], [3, 4, 7], [4, 3, 7]] # 辺集合
+# 辺を追加
+for e in test_edges:
+    test_graph.add_edge_from_index(e[0], e[1], e[2])
 
 print('デモ: prim()')
-print('隣接行列:\n', test_graph, '\n', sep='')
+print('隣接行列:\n', test_graph.generate_adj_matrix(), '\n', sep='')
 
-pred = prim(test_graph, test_graph.vertexes[0])
+pred = prim(test_graph, test_graph.vertices[0])
 MST = make_mst(test_graph, pred)
 
-print('最小被覆木:\n', MST, '\n', sep='')
+print('最小被覆木:\n', MST.generate_adj_matrix(), '\n', sep='')
 
-test_graph = graph(5)
-test_graph.init_edges(test_edges)
-test_graph.update_connection_info(is_directed=True)
+test_graph = Graph(5, directed=False)
+for e in test_edges:
+    test_graph.add_edge_from_index(e[0], e[1], e[2])
 
 print('デモ: dijkstra()')
-print('隣接行列:\n', test_graph, '\n', sep='')
+print('隣接行列:\n', test_graph.generate_adj_matrix(), '\n', sep='')
 
-prec, dist = dijkstra(test_graph, test_graph.vertexes[0])
+prec, dist = dijkstra(test_graph, test_graph.vertices[0])
 
 for dest, d in dist.items():
     print('to v_' + str(dest) + ': ' + str(d))
